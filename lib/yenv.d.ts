4cd55182baa6a71e0dc56ea390ccfd183328a019
Jeff Hansen <jeffijoe@hotmail.com>
2016-07-11 18:28:01 +0200

    trying again..

--- lib/yenv.d.ts
+++ lib/yenv.d.ts
@@ -1,39 +1,49 @@
 /**
  * To import in TypeScript:
  *
- * import yenv from 'yenv'
+ * import * as yenv from 'yenv'
  */
-
-/**
- * The environment.
- */
-export type IEnvironment = any;
-
-/**
- * The yenv options
- *
- * @interface IYenvOpts
- */
-export interface IYenvOpts {
+declare module 'yenv' {
   /**
-   * What environment should be used? Defaults to "development"
-   *
-   * @type {string}
+   * Yenv namespace
    */
-  env?: string;
+  namespace yenv {
+    /**
+     * The environment.
+     */
+    type IEnvironment = any;
+
+    /**
+     * The yenv options
+     *
+     * @interface IYenvOpts
+     */
+    interface IYenvOpts {
+      /**
+       * What environment should be used? Defaults to "development"
+       *
+       * @type {string}
+       */
+      env?: string;
+      /**
+       * The environment to check on. Defaults to `process.env`
+       *
+       * @type {IEnvironment}
+       */
+      envObject?: IEnvironment;
+    }
+  }
+
   /**
-   * The environment to check on. Defaults to `process.env`
+   * Loads the environment settings
    *
-   * @type {IEnvironment}
+   * @param {string} [filePath]
+   * @param {yenv.IYenvOpts} [opts]
+   * @returns {yenv.IEnvironment}
    */
-  envObject?: IEnvironment;
+  function yenv(filePath?: string, opts?: yenv.IYenvOpts): yenv.IEnvironment;
+
+  namespace yenv {}
+
+  export = yenv
 }
-
-/**
- * Loads the environment settings
- *
- * @param {string} [filePath]
- * @param {yenv.IYenvOpts} [opts]
- * @returns {yenv.IEnvironment}
- */
-export default function yenv(filePath?: string, opts?: IYenvOpts): IEnvironment;
